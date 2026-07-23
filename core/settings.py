aebcdf69bb4b717478a2295051bb0a40eddc366b
Lucas <lucas_wayne2023@hotmail.com>
2026-07-22 21:49:25 -0300

    fix: adiciona STATIC_ROOT correto na pasta core

--- core/settings.py
+++ core/settings.py
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# ADICIONE ESTA LINHA AQUI (Essencial para corrigir o erro):
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
